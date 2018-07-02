9a73ae50f467805cdca1ac8bdea73925faae8a2f
ragedoggie <ragedoggie@gmail.com>
2018-07-02 13:57:05 -0700

    Initial commit

---
+++ desktop/src/com/misisonbit/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.misisonbit.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.misisonbit.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}

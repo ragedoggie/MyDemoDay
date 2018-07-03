f8257e0bdf329f178c9fa293199c170adff7de0d
ragedoggie <ragedoggie@gmail.com>
2018-07-03 11:01:05 -0700

    made a sun class

---
+++ core/src/com/misisonbit/Character/Sun.java
@@ -0,0 +1,12 @@
+package com.misisonbit.Character;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Sun {
+    Texture Sun;
+
+    public Sun(){
+        Sun = new Texture("https://www.piskelapp.com/p/agxzfnBpc2tlbC1hcHByEwsSBlBpc2tlbBiAgMCf3drQCww/view\n");
+    }
+
+}
--- core/src/com/misisonbit/MyGdxGame.java
+++ core/src/com/misisonbit/MyGdxGame.java
@@ -5,15 +5,18 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.misisonbit.Character.Sun;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
 	}
 
 	@Override

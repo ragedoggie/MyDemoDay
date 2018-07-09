386cab3c7beffcb09249b08b826ac55cbc6a9bfa
hohe1 <anthonyho02015@gmail.com>
2018-07-09 09:10:42 -0700

    added States package and a few class class are empty

--- core/src/com/misisonbit/Character/Player.java
+++ core/src/com/misisonbit/Character/Player.java
@@ -2,11 +2,32 @@ package com.misisonbit.Character;
 
 public class Player {
     int trophicLevel;
+    int score;
 
     public Player() {
         trophicLevel =  1 ;
+        score = 0;
     }
+//player trophic level
+    private void evo(){
+        switch (score){
+            case 100:
+
+                break;
+            case 200:
+
+                break;
+            case 300:
+
+                break;
+            default:
 
 
+        }
+
+
+
+
+    }
 
 }

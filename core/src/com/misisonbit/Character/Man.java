30d7d996b9e478550efd9d0277d8ff6b1a3d3fb7
ragedoggie <ragedoggie@gmail.com>
2018-07-09 09:32:30 -0700

    most of animations besides wolf and human

--- core/src/com/misisonbit/Character/Man.java
+++ core/src/com/misisonbit/Character/Man.java
@@ -4,34 +4,34 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
-import com.misisonbit.Character.Organisms;
 import com.misisonbit.Sprites.Animations;
 
-    public class Rabbit extends Organisms {
-        static final int GRAVITY = 0;
-        static final int MOVEMENT = 100;
-        Vector3 position;
-        Rectangle bounds;
-        Animations rabbitAnim;
-        Texture texture;
+public class Man extends Organisms {
+
+    static final int GRAVITY = 0;
+    static final int MOVEMENT = 100;
+    Vector3 position;
+    Rectangle bounds;
+    Animations manAnim;
+    Texture texture;
 
     //Sound ray;
 
 
 
-    public Rabbit(int x, int y){
+    public Man(int x, int y){
 
         position = new Vector3(x, y, 0);
-        texture = new Texture("Rabbit.png");
+        texture = new Texture("Man.png");
         bounds = new Rectangle(x, y, texture.getWidth() / 3, texture.getHeight() / 3);
-        rabbitAnim = new Animations(new TextureRegion(texture), 2, .3f,2,2);
+        manAnim = new Animations(new TextureRegion(texture), 3, 0.5f,3,3);
 
         //ray = Gdx.audio.newSound(Gdx.files.internal("sfx_wing.ogg"));
 
     }
 
     public void update(float dt){
-        rabbitAnim.update(dt);
+        manAnim.update(dt);
 
 
     }
@@ -42,7 +42,7 @@ import com.misisonbit.Sprites.Animations;
     }
 
     public TextureRegion getTexture() {
-        return rabbitAnim.getFrame();
+        return manAnim.getFrame();
     }
 
 

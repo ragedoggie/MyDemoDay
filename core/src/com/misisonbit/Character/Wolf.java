dfb03d3deedd31f06152f108df1d46ade1daaa5a
ragedoggie <ragedoggie@gmail.com>
2018-07-18 16:14:06 -0700

    .

--- core/src/com/misisonbit/Character/Wolf.java
+++ core/src/com/misisonbit/Character/Wolf.java
@@ -1,7 +1,24 @@
-package com.misisonbit.Character;
+/*package com.misisonbit.Character;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 import com.misisonbit.Character.Organisms;
+import com.misisonbit.Sprites.Animations;
 
-//public class Wolf extends Organisms {
-//    public Wolf
-//}
+public class Wolf extends Organisms {
+    Vector3 position;
+    Rectangle bounds;
+    Animations wolfAnim;
+    Texture texture;
+
+    public  Wolf(float x,float y) {
+        position = new Vector3(x,y,0);
+        wolfAnim = new Animations(new TextureRegion(texture),);
+        bounds = new Rectangle();
+
+    }
+
+}
+*/
